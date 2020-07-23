91aed83d37c2af35cf78f109fd1cca1f92e609ed
pancake <pancake@nopcode.org>
2020-07-23 17:51:26 +0200

    C.vararg maybe is not that well supported yet

--- ns.v
+++ ns.v
@@ -5,17 +5,17 @@ import sel
 // NSAutoReleasePool
 pub fn new_autorelease_pool() Id {
 	narp_klass := get_class('NSAutoReleasePool')
-	narp := msg_send (narp_klass, sel.register('alloc'))
-	return msg_send (narp, sel.register('init'))
+	narp := msg_send2 (narp_klass, sel.register('alloc'))
+	return msg_send2 (narp, sel.register('init'))
 }
 
 pub fn (id Id)drain() {
-	msg_send(id, sel.register('drain'))
+	msg_send2(id, sel.register('drain'))
 }
 
 // NSString
 pub fn new_nsstring(a string) Id {
 	nss_klass := get_class('NSString')
 	init := sel.register('stringWithUTF8String:')
-	return msg_send (nss_klass, init, byteptr(a.str))
+	return msg_send3 (nss_klass, init, byteptr(a.str))
 }
--- objc.v
+++ objc.v
@@ -9,7 +9,9 @@ type Id voidptr
 type Sel byteptr
 type Class voidptr
 
-type MsgSend fn (Id, Sel, ...voidptr) voidptr
+type MsgSend2 fn (Id, Sel) voidptr
+type MsgSend3 fn (Id, Sel, Id) voidptr
+type MsgSend4 fn (Id, Sel, Id, Id) voidptr
 
 // fn C.UIApplicationMain(int, ...)
 
@@ -17,18 +19,21 @@ fn C.objc_getClass(charptr) Class
 // fn C.objc_msgSend(voidptr, charptr, ...voidptr) int
 // fn C.objc_msgSend_stret(voidptr, charptr, ...voidptr) int
 
-pub fn msg_send_super(a Id, msg string, args ...voidptr) Id {
-	send := MsgSend(C.objc_msgSendSuper)
-	r := send (a, charptr(msg.str), args)
-	return Id(r)
-}
-
 pub fn get_class(klass string) Class {
 	return Id(C.objc_getClass(klass.str))
 }
 
-pub fn msg_send(a Id, msg Sel, args ...voidptr) Id {
-	send := MsgSend(C.objc_msgSend)
-	r := send (a, msg, args)
-	return Id(r)
+pub fn msg_send4(a Id, msg Sel, arg Id, arg2 Id) Id {
+	send := MsgSend4(C.objc_msgSend)
+	return send (a, msg, arg, arg2)
+}
+
+pub fn msg_send3(a Id, msg Sel, arg Id) Id {
+	send := MsgSend3(C.objc_msgSend)
+	return send (a, msg, arg)
+}
+
+pub fn msg_send2(a Id, msg Sel) Id {
+	send := MsgSend2(C.objc_msgSend)
+	return send (a, msg)
 }
