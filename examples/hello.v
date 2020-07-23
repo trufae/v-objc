f43693efdc45fd07c4685e5a0fa135aa216be6ee
pancake <pancake@nopcode.org>
2020-07-23 22:04:01 +0200

    a, b

--- examples/hello.v
+++ examples/hello.v
@@ -4,7 +4,8 @@ fn main() {
 	ap := objc.new_autorelease_pool()
 	a := objc.new_nsstring('hello world')
 	println('$a') // automatic .str() unwrapping
-	a.append(' good bye')
-	println('$a') // automatic .str() unwrapping
+	println('$a') // try double-free
+	b := a.append(' good bye')
+	println('$b') // automatic .str() unwrapping
 	ap.drain()
 }
